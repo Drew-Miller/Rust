13e062ab198d75305e70f8a076785898bc23b024
Drew Miller <d.m.lespaul@gmail.com>
2022-01-27 14:22:50 -0800

    init

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    let mut count = 0u32;
+    let mut guesses: Vec<u32> = Vec::new();
+
+    loop {
+        count += 1;
+
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        guesses.push(guess);
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+
+    println!("You guessed the number in {} tries!", count);
+    for guess in &guesses {
+        print!("{} ", guess);
+    }
+    print!("\n");
+
+    println!("Goodbye!");
+}
